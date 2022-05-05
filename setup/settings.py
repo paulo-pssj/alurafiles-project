382780a41dab6255c0e852e9190fe2a148d367c3
paulo-pssj <paulinhops99@gmail.com>
2022-05-05 11:39:07 -0300

    Deploy

--- setup/settings.py
+++ setup/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 import django_heroku
 
 # config heroku
-django_heroku.settings(locals())
+django_heroku.settings(locals(), staticfiles=False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
